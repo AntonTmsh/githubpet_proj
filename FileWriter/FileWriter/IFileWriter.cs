ed795088b7816effa344469f913d4b88d9452d82
anton <ti1@ukr.net>
2018-10-09 19:17:18 +0300

    Disposable

---
+++ FileWriter/FileWriter/IFileWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace Epam.NetMentoring.Disposable
+{
+    public interface IFileWriter
+    {
+        void Write(string str);
+        void WriteLine(string str);
+    }
+}
---
+++ IDisposableImplementation/IDisposableImplementation/MemoryStreamLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Epam.NetMentoring.Disposable
+{
+    public class MemoryStreamLogger: IDisposable 
+    {
+        private FileStream memoryStream;
+        private StreamWriter streamWriter;
+
+        public MemoryStreamLogger()
+        {
+            memoryStream = new FileStream(@"\log.txt", FileMode.OpenOrCreate);
+            streamWriter = new StreamWriter(memoryStream);
+        }
+
+        public void Log(string message)
+        {
+            streamWriter.Write(message);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (memoryStream != null) memoryStream.Dispose();
+                if (streamWriter != null) streamWriter.Dispose();
+            }
+        }
+
+    }
+}
